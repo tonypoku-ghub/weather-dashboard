492fbc7f70cb4a4fac8372c45a4c6a5c07f13f74
Anthony Poku <tonypoku@gmail.com>
2022-05-14 03:52:03 -0500

    Save to localstorage

--- assets/js/script_async.js
+++ assets/js/script_async.js
@@ -1,9 +1,11 @@
+var search_history_array = [];
+
 var searchHistoryEl = document.querySelector("#search-history");
 
 var searchInputEl = document.querySelector("#search-input");
 
 var weatherURL =
-  "https://api.openweathermap.org/data/2.5/weather?q={city}&appid=dff9798285dea32133f4c2c13f702b52";
+  "https://api.openweathermap.org/data/2.5/weather?units=imperial&q={city}&appid=dff9798285dea32133f4c2c13f702b52";
 
 var uvindexURL =
   "https://api.openweathermap.org/data/2.5/onecall?units=imperial&lat={lat}&lon={lon}&exclude=hourly,daily,minutely,alerts&appid=dff9798285dea32133f4c2c13f702b52";
@@ -16,10 +18,11 @@ function searchWeatherFromInput() {
 }
 
 function searchWeatherFromHistory() {
-  searchWeather(this.attr("id"));
+  searchWeather($(this).attr("id"));
 }
 
 async function searchWeather(city) {
+  city = city.toUpperCase();
   var searchURL = weatherURL.replace("{city}", city);
   var response = await fetch(searchURL);
 
@@ -30,6 +33,8 @@ async function searchWeather(city) {
   updateDashboard(city, data);
 
   logSearchHistory(city);
+
+  saveToLocalStorage(city);
 }
 
 async function getUVIndex(lat, lon) {
@@ -70,28 +75,33 @@ async function updateDashboard(city, data) {
 
   var daily = await getDailyForecast(data.coord.lat, data.coord.lon);
 
+  // Update header with city name
+  $("#city_header")
+    .css("font-weight", "bold")
+    .text(city + moment.unix(data.dt).format("(MM/DD/YYYY)"));
+
   // Remove old cards
   $("#city-5-day").empty();
 
   daily.forEach((day, index) => {
     if (index < 5) {
-      var dt = moment.unix(day.dt).format("(MM/DD/YYYY)");
+      var dt = moment.unix(day.dt).format("MM/DD/YYYY");
       var temp = day.temp.day;
       var wind = day.wind_speed;
       var humidity = day.humidity;
       var icon = day.weather[0].icon;
 
-      addWeatherCard(city, dt, temp, wind, humidity, icon);
+      addWeatherCard(dt, temp, wind, humidity, icon);
     }
   });
 }
 
-function addWeatherCard(city, date, temp, wind, humidity, icon) {
+function addWeatherCard(date, temp, wind, humidity, icon) {
   var cardDiv = $("<div>").addClass("card w-25 m-2");
 
   var cardHeaderDiv = $("<div>")
     .addClass("card-header")
-    .text(city + " " + date)
+    .text(date)
     .css("font-weight", "bold");
 
   var cardImageDiv = $("<img>")
@@ -151,9 +161,40 @@ function logSearchHistory(city) {
     .find("#" + city)
     .remove();
 
-  $(searchHistoryEl).append(newContentBtn);
+  $(searchHistoryEl).prepend(newContentBtn);
 }
 
 $(function () {
   $("#btn-search").click(searchWeatherFromInput);
+
+  doInit();
 });
+
+function saveToLocalStorage(city) {
+  var elementIndex = $.inArray(city, search_history_array);
+
+  if (elementIndex > -1) {
+    search_history_array.splice(elementIndex, 1);
+  }
+
+  search_history_array.push(city);
+
+  localStorage.setItem(
+    "weather-search-history",
+    JSON.stringify(search_history_array)
+  );
+}
+
+function doInit() {
+  search_history_array = JSON.parse(
+    localStorage.getItem("weather-search-history")
+  );
+
+  if (!search_history_array) {
+    search_history_array = [];
+  }
+
+  search_history_array.forEach((city) => {
+    logSearchHistory(city);
+  });
+}
